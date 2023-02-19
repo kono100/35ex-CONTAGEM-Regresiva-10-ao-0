4edd547510ab2e18914f8d1877b09841e1b08974
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-19 03:40:18 -0300

    Add files via upload

---
+++ 35ex CONTAGEM Regresiva 10 ao 0.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+#35. Elabore um programa que imprima na tela uma contagem regressiva de 10 até 0
+
+
+import time
+
+contador = 10
+while (contador > -1):
+       time.sleep(0.2)
+       print(contador)
+       contador   = contador - 1
